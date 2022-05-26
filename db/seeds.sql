bf842e230a259a7b7ab4511bf77881fe56c76c63
Leo Wong <leoelicos@gmail.com>
2022-05-26 10:58:31 +1000

    Starter code from BCS

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO movies (movie_name)
+VALUES ("Godfather"),
+       ("Godfather 2"),
+       ("Godfather 3");
+
+INSERT INTO reviews (movie_id, review)
+VALUES (001, "The best movie ever"),
+       (002, "The second-best movie ever"),
+       (003, "The worst movie ever");
